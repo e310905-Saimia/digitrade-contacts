5f1603328321081370cbcc9629a6cbe532fe855f
e310905-saimia <jouni.k.kononen@gmail.com>
2019-05-09 12:12:08 +0300

    Updated

--- ContactsWebApi/Controllers/ContactsController.cs
+++ ContactsWebApi/Controllers/ContactsController.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ContactsWebApi.Controllers
-{
+{    
     [Route("api/[controller]")]
     [ApiController]
     public class ContactsController : ControllerBase
